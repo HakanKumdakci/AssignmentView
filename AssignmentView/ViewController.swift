d1966339f0620189e31c2b6c7ad3849569384e7c
HakanKumdakci <hakankumdakci@gmail.com>
2022-04-28 23:38:03 +0300

    Initial Commit

---
+++ AssignmentView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentView
+//
+//  Created by Hakan Kumdakçı on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
